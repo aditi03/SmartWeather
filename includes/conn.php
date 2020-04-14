abed33e48dd09dbc907d3a07d39315e5e252f7d1
saahilchawande <chawandesaahil600@gmail.com>
2020-04-14 17:22:37 -0400

    Updated conn.php to original functionality

--- includes/conn.php
+++ includes/conn.php
@@ -1,4 +1,21 @@
 <?php
+//
+//$user = 'root';
+//$password = 'root';
+//$db = 'smartweather';
+//$host = 'localhost';
+//$port = 8889;
+//
+//// Create connection
+//$conn = mysqli_init();
+//$connection = mysqli_real_connect($conn, $host, $user, $password, $db, $port);
+//
+//// Check connection
+//if(!$conn)  {
+//    die("Connection failed: " . mysqli_connect_error());
+//}
+//# echo "Connected successfully";
+
 
 $servername = "localhost";
 $username = "root";
@@ -9,7 +26,7 @@ $database_name = "smartweather";
 $conn = mysqli_connect($servername, $username, $password, $database_name);
 
 // Check connection
-if(!$conn)  {
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 # echo "Connected successfully";
